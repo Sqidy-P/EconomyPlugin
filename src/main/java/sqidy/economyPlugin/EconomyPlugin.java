48a49064f49373d053ec32b48f1e6a340d004307
Sqidy <sqidyp@gmail.com>
2025-02-11 01:30:51 -0500

    Balance command and FirstJoin listener added.

--- src/main/java/sqidy/economyPlugin/EconomyPlugin.java
+++ src/main/java/sqidy/economyPlugin/EconomyPlugin.java
@@ -1,14 +1,13 @@
 package sqidy.economyPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import sqidy.economyPlugin.commands.Balance;
 import sqidy.economyPlugin.handlers.FileHandler;
+import sqidy.economyPlugin.listeners.FirstJoinListener;
 
 
 public final class EconomyPlugin extends JavaPlugin {
     public EconomyPlugin plugin;
-
-    public String configDir = "./plugins/EconomyPlugin/config.yml";
-    public String accountsDir = "./plugins/EconomyPlugin/accounts.yml";
 
     @Override
     public void onEnable() {
@@ -17,22 +16,13 @@ public final class EconomyPlugin extends JavaPlugin {
         this.getLogger().info("Sqidy's Economy plugin has loaded.");
 
         // Commands
+        this.getCommand("balance").setExecutor(new Balance());
 
         // Listeners
+        new FirstJoinListener(this);
 
         //  Handlers
         FileHandler.setup();
-
-        //region Debugging
-        // Add account
-        FileHandler.modifyAccountData("TestUUID", "0.00", true);
-        FileHandler.modifyAccountData("TestUUIDTwo", "0.00", true);
-        // Add x float amount to x user (/pay <amount> <optional IGN>)
-        FileHandler.addToBalance("TestUUID", 0.303F);
-        FileHandler.addToBalance("TestUUIDTwo", 21.03F);
-        // Sets a player's balance (/setBal <IGN> <amount>)
-        FileHandler.modifyAccountData("TestUUIDTwo", "50.00", false);
-        //endregion
     }
 
     @Override
