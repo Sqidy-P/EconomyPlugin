fa8a501954432fa890b04440a3bb8b4dadf9c064
Sqidy <sqidyp@gmail.com>
2025-02-14 23:27:48 -0500

    Added utils for miscellaneous methods and variables.

---
+++ src/main/java/sqidy/economyPlugin/utils/EconomyUtils.java
@@ -0,0 +1,15 @@
+package sqidy.economyPlugin.utils;
+
+public class EconomyUtils {
+    static String configDir = "./plugins/EconomyPlugin/config.yml";
+    static public String accountsDir = "./plugins/EconomyPlugin/accounts.yml";
+
+    public static boolean isFloat(String str){
+        try {
+            Float.parseFloat(str);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
