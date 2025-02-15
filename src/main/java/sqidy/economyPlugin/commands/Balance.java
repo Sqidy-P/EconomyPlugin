4ce5ea378cdc343138d88aca1e17208e93c189ca
Sqidy <sqidyp@gmail.com>
2025-02-14 23:26:41 -0500

    Removed if statements for switch statements. Abstracted the code.

--- src/main/java/sqidy/economyPlugin/commands/Balance.java
+++ src/main/java/sqidy/economyPlugin/commands/Balance.java
@@ -1,60 +1,58 @@
 package sqidy.economyPlugin.commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import sqidy.economyPlugin.handlers.FileHandler;
+
+import static sqidy.economyPlugin.handlers.BalanceHandler.*;
+import static sqidy.economyPlugin.utils.EconomyUtils.isFloat;
 
 public class Balance implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = (Player) sender;
-        String playerUUID = player.getUniqueId().toString();
+        if (!(sender instanceof Player)){
+            sender.sendMessage("You must be a player to preform this command!");
+        }
 
-        // If no args, return user balance
-        if (args.length == 0){
-            String currentBalance = FileHandler.getBalance(playerUUID);
+        Player player = (Player) sender;
+
+        if (args.length == 3 && isFloat(args[2])){
+            float amount = Float.parseFloat(args[2]);
+            String targetPlayer = args[1];
+
+            switch (args[0].toLowerCase()){
+                case "give":
+                    if (player.hasPermission("economy.give")) {
+                        addToBalance(targetPlayer, amount);
+                        return true;
+                    }
+                case "take":
+                    if (player.hasPermission("economy.take")){
+                        takeFromBalance(targetPlayer, amount);
+                        return true;
+                    }
+                default:
+                    return false;
+            }
+        }
+        else if (args.length == 2
+                && args[0].equalsIgnoreCase("see")
+                && player.hasPermission("economy.see")){
+
+
+            String targetPlayer = args[1];
+            String targetPlayerBalance = getBalance(targetPlayer);
+
+            player.sendMessage(targetPlayer + " currently has " + targetPlayerBalance + "!");
+            return true;
+        }
+        else if (args.length == 0){
+            String currentBalance = getBalance(player.getName());
             player.sendMessage("You currently have $" + currentBalance + "!");
             return true;
         }
 
-        // If 1 arg, return false;
-        // unneeded. it'll do it automatically
-        player.sendMessage("Balance command used with " + args.length);
-        // If 3 arg
-        if (args.length == 3) {
-            // check what arg and permission
-            if (args[0].equalsIgnoreCase("give") && player.hasPermission("economy.give")){
-                Bukkit.getLogger().info("GIVE COMMAND USED");
-
-                String TargetPlayer;
-                String TargetPlayerUUID;
-
-                if (Bukkit.getPlayer(args[1]).isOnline()){
-                    Bukkit.getLogger().info("PLAYER IS ONLINE");
-                    TargetPlayer = Bukkit.getPlayer(args[1]).getName();
-                    TargetPlayerUUID = Bukkit.getPlayerUniqueId(args[1]).toString();
-
-                    if (!(Float.parseFloat(args[2]) >= 0.00)){
-                        Bukkit.getLogger().info("NUMBER IS NOT FLOAT");
-                        return false;
-                    }
-
-                    float amountToAdd = Float.parseFloat(args[2]);
-
-                    FileHandler.addToBalance(TargetPlayerUUID, TargetPlayer, amountToAdd);
-
-                    Bukkit.getLogger().info("MONEY ADDED");
-                    return true;
-                }
-
-                return false;
-            }
-            // if they have permission, and there is an ign, follow through
-        }
-
-        return true;
+        return false;
     }
 }
